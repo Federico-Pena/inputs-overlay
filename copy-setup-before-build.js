945996e90ab911487ea0b9ed0e9d07f7761d9363
Federico Pena <federpena22@gmail.com>
2024-10-23 21:49:17 -0300

    setup SFX with password

--- copy-setup-before-build.js
+++ copy-setup-before-build.js
@@ -1,11 +1,23 @@
 const { join } = require('node:path')
-const { rm, mkdir } = require('node:fs/promises')
+const { rm, mkdir, writeFile } = require('node:fs/promises')
 const { existsSync } = require('node:fs')
 const { exec } = require('node:child_process')
 
-const compressAndCopyFile = async (source, destination) => {
+const createPasswordFile = async (frontendDestinationDir, password) => {
+  const passwordFilePath = join(frontendDestinationDir, 'password.json')
+  const content = {
+    password: password
+  }
+
+  await writeFile(passwordFilePath, JSON.stringify(content, null, 2), 'utf-8')
+  console.log(`Archivo de contraseña creado en: ${passwordFilePath}`)
+}
+
+const compressAndCopyFile = async (source, destination, password) => {
   return new Promise((resolve, reject) => {
-    const command = `powershell Compress-Archive -Force -Path "${source}" -DestinationPath "${destination}"`
+    // const command = `powershell Compress-Archive -Force -Path "${source}" -DestinationPath "${destination}"`
+    // const command = `rar a -ep -hp${password} "${destination}" "${source}"`
+    const command = `rar a -ep -hp${password} -sfx "${destination}" "${source}"`
     exec(command, (error, stdout, stderr) => {
       if (error) {
         reject(`Error durante la compresión: ${error.message}`)
@@ -22,7 +34,7 @@ exports.default = async function (context) {
   try {
     const source = context.artifactPaths[1]
     const setupName = context.artifactPaths[1].split('\\').pop()
-    const destinationDir = join(
+    const frontendDestinationDir = join(
       process.cwd(),
       'page-for-download',
       'public',
@@ -33,14 +45,17 @@ exports.default = async function (context) {
       throw new Error(`El archivo ${source} no existe.`)
     }
 
-    if (!existsSync(destinationDir)) {
-      await mkdir(destinationDir, { recursive: true })
-      console.log(`Carpeta creada: ${destinationDir}`)
+    if (!existsSync(frontendDestinationDir)) {
+      await mkdir(frontendDestinationDir, { recursive: true })
+      console.log(`Carpeta creada: ${frontendDestinationDir}`)
     }
 
-    const compressedFileName = `${setupName.split('.exe')[0]}.zip`
-    const destination = join(destinationDir, compressedFileName)
-    const result = await compressAndCopyFile(source, destination)
+    const compressedFileName = `${setupName.split('.exe')[0]}`
+    const destination = join(frontendDestinationDir, compressedFileName)
+    const password = setupName.split('-')[0]
+
+    const result = await compressAndCopyFile(source, destination, password)
+    await createPasswordFile(frontendDestinationDir, password)
     console.log(result)
 
     await rm(context.outDir, { recursive: true, force: true })
